153a8d18fdcad2a0ff6f3382c53cec08e682976f
Ross Bamford <roscopeco@gmail.com>
2023-10-22 21:22:39 +0100

    Fix warnings and remove old debugging stuff

--- main.cpp
+++ main.cpp
@@ -98,7 +98,7 @@ extern "C" {
     rosco::m68k::emu::AddressDecoder* __mem;
     std::fstream ifs("rosco_sd.bin", std::ios::binary | std::ios::ate | std::ios::in | std::ios::out);
 
-    int illegal_instruction_handler(int opcode) {
+    int illegal_instruction_handler(int __attribute__((unused)) opcode) {
         m68ki_cpu_core ctx;
         m68k_get_context(&ctx);
 
@@ -526,60 +526,15 @@ int main(int argc, char** argv) {
 
         m68ki_cpu_core ctx;
         m68k_get_context(&ctx);
-
-#ifdef	DEBUG
-        cout << "Address mask: 0x" << hex << ctx.address_mask << endl;
-        cout << "Cpu type    : 0x" << ctx.cpu_type << endl << endl;
-
-        uint32_t memWord = m68k_read_memory_32(0x40000);
-        uint32_t memWord2 = m68k_read_memory_32(0xfc0004);
-        uint32_t memWord3 = m68k_read_memory_32(0);
-
-        uint32_t biosMagic = m68k_read_memory_32(0x400);
-        uint32_t oshiCode = m68k_read_memory_32(0x404);
-
-        cout << "PC          : 0x" << m68k_get_reg(&ctx, M68K_REG_PC) << endl;
-        cout << "BIOS Magic  : 0x" << biosMagic << endl;
-        cout << "OSHI Code   : 0x" << oshiCode << endl;
-        cout << "mem[0]      : 0x" << memWord3 << endl;
-        cout << "mem[0x40000]: 0x" << memWord << endl;
-        cout << "mem[fc0004] : 0x" << memWord2 << endl;
-        cout << "SR          : 0x" << m68k_get_reg(&ctx, M68K_REG_SR) << endl;
-        cout << "D0          : 0x" << m68k_get_reg(&ctx, M68K_REG_D0) << endl;
-        cout << "D7          : 0x" << m68k_get_reg(&ctx, M68K_REG_D7) << endl << endl;
-#		endif
         
         std::thread timer_thread(timer_interrupt);
-        int cycles = 0;
-        while (1) { //!ctx.stopped) {
-            cycles += m68k_execute(100000);
+        while (1) {
+            m68k_execute(100000);
             m68k_get_context(&ctx);
         }
 
         is_done = true;
         timer_thread.join();
-
-#ifdef	DEBUG
-        cout << endl;
-        cout << "Executed    : " << dec << cycles << " cycle(s)" << hex << endl << endl;
-
-        memWord = m68k_read_memory_32(0xfc0000);
-        memWord2 = m68k_read_memory_32(0xfc0004);
-        memWord3 = m68k_read_memory_32(0);
-
-        biosMagic = m68k_read_memory_32(0x400);
-        oshiCode = m68k_read_memory_32(0x404);
-
-        cout << "PC          : 0x" << m68k_get_reg(&ctx, M68K_REG_PC) << endl;
-        cout << "BIOS Magic  : 0x" << biosMagic << endl;
-        cout << "OSHI Code   : 0x" << oshiCode << endl;
-        cout << "mem[0]      : 0x" << memWord3 << endl;
-        cout << "mem[fc0000] : 0x" << memWord << endl;
-        cout << "mem[fc0004] : 0x" << memWord2 << endl;
-        cout << "SR          : 0x" << m68k_get_reg(&ctx, M68K_REG_SR) << endl;
-        cout << "D0          : 0x" << m68k_get_reg(&ctx, M68K_REG_D0) << endl;
-        cout << "D7          : 0x" << m68k_get_reg(&ctx, M68K_REG_D7) << endl;
-#		endif
 
         delete(__mem);
         return 0;
